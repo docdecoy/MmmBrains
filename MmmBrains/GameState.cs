428b13d63ffc27b75914af6d41a600332595dbe4
docdecoy <shane.tapp@charter.net>
2015-02-19 21:00:25 +0000

    Missing Class

--- MmmBrains/GameState.cs
+++ MmmBrains/GameState.cs
@@ -9,7 +9,31 @@ namespace MmmBrains
     class GameState
     {
 
-
+                // Roll my dice (TODO: do something with this?)
+            //foreach (var dice in diceInHand)
+            //{
+            //    var diceRollResult = dice.Roll();
+            //    switch (diceRollResult)
+            //    {
+            //        case DiceFaceImage.GreenBrain:
+            //        case DiceFaceImage.YellowBrain:
+            //        case DiceFaceImage.RedBrain:
+            //            _rolledBrains.Add(diceRollResult);
+            //            break;
+            //        case DiceFaceImage.GreenFeet:
+            //        case DiceFaceImage.YellowFeet:
+            //        case DiceFaceImage.RedFeet:
+            //            _rolledFeet.Add(diceRollResult);
+            //            break;
+            //        case DiceFaceImage.GreenShotgun:
+            //        case DiceFaceImage.YellowShotgun:
+            //        case DiceFaceImage.RedShotgun:
+            //            _rolledShotguns.Add(diceRollResult);
+            //            break;
+            //        default:
+            //            throw new ArgumentOutOfRangeException();
+            //    }
+            //}
 
     }
 }
